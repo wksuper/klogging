4456dde21f6f353538c5df1f1225f33a4335ce24
Kui Wang <wangkuisuper@hotmail.com>
2020-11-27 20:54:26 +0800

    Minor modification for runmyprog.sh
    Signed-off-by: Kui Wang <wangkuisuper@hotmail.com>

--- example/runmyprog.sh
+++ example/runmyprog.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -v
-
-./myprog KLOG_SET_OPTIONS=0x0101 KLOG_SET_LEVEL=5 KLOG_SET_LINEEND=" custom string\n" KLOG_SET_FILE=mylog.txt
+./myprog KLOG_SET_OPTIONS=0x0101 KLOG_SET_LEVEL=5 KLOG_SET_LINEEND=" custom line end string\n" KLOG_SET_FILE=mylog.txt
